c8b6cfee192aec3156121b3174ac9f38b1063932
Mahmmed_Ahmed <wwwmahmmed375@gmail.com>
2022-06-05 21:53:07 +0200

    تعديلات ما قبل الرفع

--- js/Hone.js
+++ js/Hone.js
@@ -1,138 +1,18 @@
-let on = document.getElementsByTagName("a")[6];
-let on1 = document.getElementsByTagName("a")[7];
-let on2 = document.getElementsByTagName("a")[8];
-let on3 = document.getElementsByTagName("a")[9];
-let on5 = document.getElementsByTagName("a")[10];
+let hea =document.getElementById("h"),
+            ul2=document.getElementById("ul"),
+            width=document.getElementById("width"),
+            width2=document.getElementById("width2");
 
-
-let sre = ["../img/Home/1/1.jpg","../img/Home/2/1.jpg","../img/Home/3/1.jpg","../img/Home/4/1.jpg","../img/Home/5=1/1.jpg"];
-let sre1 = ["../img/Home/1/2.jpg","../img/Home/2/2.jpg","../img/Home/3/2.jpg","../img/Home/4/2.jpg","../img/Home/2.jpg"];
-let sre2 = ["../img/Home/1/3.jpg","../img/Home/2/3.jpg","../img/Home/3/3.jpg","../img/Home/4/3.jpg","../img/Home/3.jpg"];
-let sre3 = ["../img/Home/1/4.jpg","../img/Home/2/4.jpg","../img/Home/3/4.jpg","../img/Home/4/4.jpg","../img/Home/4.jpg"];
-let sre4 = ["../img/Home/1/5.jpg","../img/Home/2/5.jpg","../img/Home/3/5.jpg","../img/Home/4/5.jpg","../img/Home/5.jpg"];
-let sre5 = ["../img/Home/1/6.jpg","../img/Home/2/6.jpg","../img/Home/3/6.jpg","../img/Home/4/6.jpg","../img/Home/5=1/6.jpg"];
-let sre6 = ["../img/Home/1/7.jpg","../img/Home/2/7.jpg","../img/Home/3/7.jpg","../img/Home/4/7.jpg","../img/Home/5=1/7.jpg"];
-let sre7 = ["../img/Home/1/8.jpg","../img/Home/2/8.jpg","../img/Home/3/8.jpg","../img/Home/4/8.jpg","../img/Home/5=1/8.jpg"];
-let sre8 = ["../img/Home/1/9.jpg","../img/Home/2/9.jpg","../img/Home/3/9.jpg","../img/Home/4/9.jpg","../img/Home/5=1/9.jpg"];
-let sre9 = ["../img/Home/1/10.jpg","../img/Home/2/10.jpg","../img/Home/3/10.jpg","../img/Home/4/10.jpg","../img/Home/5=1/10.jpg"];
-let sre10 = ["../img/Home/1/11.jpg","../img/Home/2/11.jpg","../img/Home/3/11.jpg","../img/Home/4/11.jpg","../img/Home/5=1/11.jpg"];
-let sre11 = ["../img/Home/1/12.jpg","../img/Home/2/12.jpg","../img/Home/3/12.jpg","../img/Home/4/12.jpg","../img/Home/5=1/12.jpg"];
-let sre12 = ["../img/Home/1/13.jpg","../img/Home/2/13.jpg","../img/Home/3/13.jpg","../img/Home/4/13.jpg","../img/Home/5=1/13.jpg"];
-let sre13 = ["../img/Home/1/14.jpg","../img/Home/2/14.jpg","../img/Home/3/14.jpg","../img/Home/4/14.jpg","../img/Home/5=1/14.jpg"];
-let sre14 = ["../img/Home/1/15.jpg","../img/Home/2/15.jpg","../img/Home/3/15.jpg","../img/Home/4/15.jpg","../img/Home/5=1/15.jpg"];
-let sre15 = ["../img/Home/1/16.jpg","../img/Home/2/16.jpg","../img/Home/3/16.jpg","../img/Home/4/16.jpg","../img/Home/5=1/16.jpg"];
-let sre16 = ["../img/Home/1/17.jpg","../img/Home/2/17.jpg","../img/Home/3/17.jpg","../img/Home/4/17.jpg","../img/Home/5=1/17.jpg"];
-let sre17 = ["../img/Home/1/18.jpg","../img/Home/2/18.jpg","../img/Home/3/18.jpg","../img/Home/4/18.jpg","../img/Home/5=1/18.jpg"];
+            hea.onclick=function () {
+  
+            ul2.classList.toggle ("ul");
+            width.classList.toggle ("fil");
+            width2.classList.toggle ("fil");
+           }
 
 
 
-on1.onclick= function () {
-
-    document.getElementsByClassName("prodect")[1].style.backgroundImage=` url(${sre1[0]})`
-    document.getElementsByClassName("prodect")[2].style.backgroundImage=` url(${sre2[0]})`
-    document.getElementsByClassName("prodect")[3].style.backgroundImage=` url(${sre3[0]})`
-    document.getElementsByClassName("prodect")[4].style.backgroundImage=` url(${sre4[0]})`
-    document.getElementsByClassName("prodect")[5].style.backgroundImage=` url(${sre5[0]})`
-    document.getElementsByClassName("prodect")[6].style.backgroundImage=` url(${sre6[0]})`
-    document.getElementsByClassName("prodect")[7].style.backgroundImage=` url(${sre7[0]})`
-    document.getElementsByClassName("prodect")[8].style.backgroundImage=` url(${sre8[0]})`
-    document.getElementsByClassName("prodect")[9].style.backgroundImage=` url(${sre9[0]})`
-    document.getElementsByClassName("prodect")[10].style.backgroundImage=` url(${sre10[0]})`
-    document.getElementsByClassName("prodect")[11].style.backgroundImage=` url(${sre11[0]})`
-    document.getElementsByClassName("prodect")[12].style.backgroundImage=` url(${sre12[0]})`
-    document.getElementsByClassName("prodect")[13].style.backgroundImage=` url(${sre13[0]})`
-    document.getElementsByClassName("prodect")[14].style.backgroundImage=` url(${sre14[0]})`
-    document.getElementsByClassName("prodect")[15].style.backgroundImage=` url(${sre15[0]})`
-    document.getElementsByClassName("prodect")[16].style.backgroundImage=` url(${sre16[0]})`
-    document.getElementsByClassName("prodect")[17].style.backgroundImage=` url(${sre17[0]})`
-    document.getElementsByClassName("prodect")[0].style.backgroundImage=` url(${sre[0]})`
-    
-}
-on2.onclick= function () {
-    document.getElementsByClassName("prodect")[1].style.backgroundImage=` url(${sre1[1]})`
-    document.getElementsByClassName("prodect")[2].style.backgroundImage=` url(${sre2[1]})`
-    document.getElementsByClassName("prodect")[3].style.backgroundImage=` url(${sre3[1]})`
-    document.getElementsByClassName("prodect")[4].style.backgroundImage=` url(${sre4[1]})`
-    document.getElementsByClassName("prodect")[5].style.backgroundImage=` url(${sre5[1]})`
-    document.getElementsByClassName("prodect")[6].style.backgroundImage=` url(${sre6[1]})`
-    document.getElementsByClassName("prodect")[7].style.backgroundImage=` url(${sre7[1]})`
-    document.getElementsByClassName("prodect")[8].style.backgroundImage=` url(${sre8[1]})`
-    document.getElementsByClassName("prodect")[9].style.backgroundImage=` url(${sre9[1]})`
-    document.getElementsByClassName("prodect")[10].style.backgroundImage=` url(${sre10[1]})`
-    document.getElementsByClassName("prodect")[11].style.backgroundImage=` url(${sre11[1]})`
-    document.getElementsByClassName("prodect")[12].style.backgroundImage=` url(${sre12[1]})`
-    document.getElementsByClassName("prodect")[13].style.backgroundImage=` url(${sre13[1]})`
-    document.getElementsByClassName("prodect")[14].style.backgroundImage=` url(${sre14[1]})`
-    document.getElementsByClassName("prodect")[15].style.backgroundImage=` url(${sre15[1]})`
-    document.getElementsByClassName("prodect")[16].style.backgroundImage=` url(${sre16[1]})`
-    document.getElementsByClassName("prodect")[17].style.backgroundImage=` url(${sre17[1]})`
-    document.getElementsByClassName("prodect")[0].style.backgroundImage=` url(${sre[1]})`
-    d
-}
-on3.onclick= function () {
-    document.getElementsByClassName("prodect")[1].style.backgroundImage=` url(${sre1[2]})`
-    document.getElementsByClassName("prodect")[2].style.backgroundImage=` url(${sre2[2]})`
-    document.getElementsByClassName("prodect")[3].style.backgroundImage=` url(${sre3[2]})`
-    document.getElementsByClassName("prodect")[4].style.backgroundImage=` url(${sre4[2]})`
-    document.getElementsByClassName("prodect")[5].style.backgroundImage=` url(${sre5[2]})`
-    document.getElementsByClassName("prodect")[6].style.backgroundImage=` url(${sre6[2]})`
-    document.getElementsByClassName("prodect")[7].style.backgroundImage=` url(${sre7[2]})`
-    document.getElementsByClassName("prodect")[8].style.backgroundImage=` url(${sre8[2]})`
-    document.getElementsByClassName("prodect")[9].style.backgroundImage=` url(${sre9[2]})`
-    document.getElementsByClassName("prodect")[10].style.backgroundImage=` url(${sre10[2]})`
-    document.getElementsByClassName("prodect")[11].style.backgroundImage=` url(${sre11[2]})`
-    document.getElementsByClassName("prodect")[12].style.backgroundImage=` url(${sre12[2]})`
-    document.getElementsByClassName("prodect")[13].style.backgroundImage=` url(${sre13[2]})`
-    document.getElementsByClassName("prodect")[14].style.backgroundImage=` url(${sre14[2]})`
-    document.getElementsByClassName("prodect")[15].style.backgroundImage=` url(${sre15[2]})`
-    document.getElementsByClassName("prodect")[16].style.backgroundImage=` url(${sre16[2]})`
-    document.getElementsByClassName("prodect")[17].style.backgroundImage=` url(${sre17[2]})`
-    document.getElementsByClassName("prodect")[0].style.backgroundImage=` url(${sre[2]})`
-    d
-}
-on5.onclick= function () {
-    document.getElementsByClassName("prodect")[1].style.backgroundImage = ` url(${sre1[3]})`;
-    document.getElementsByClassName("prodect")[2].style.backgroundImage = ` url(${sre2[3]})`;
-    document.getElementsByClassName("prodect")[3].style.backgroundImage = ` url(${sre3[3]})`;
-    document.getElementsByClassName("prodect")[4].style.backgroundImage = ` url(${sre4[3]})`;
-    document.getElementsByClassName("prodect")[5].style.backgroundImage = ` url(${sre5[3]})`;
-    document.getElementsByClassName("prodect")[6].style.backgroundImage = ` url(${sre6[3]})`;
-    document.getElementsByClassName("prodect")[7].style.backgroundImage = ` url(${sre7[3]})`;
-    document.getElementsByClassName("prodect")[8].style.backgroundImage = ` url(${sre8[3]})`;
-    document.getElementsByClassName("prodect")[9].style.backgroundImage = ` url(${sre9[3]})`;
-    document.getElementsByClassName("prodect")[10].style.backgroundImage = ` url(${sre10[3]})`;
-    document.getElementsByClassName("prodect")[11].style.backgroundImage = ` url(${sre11[3]})`;
-    document.getElementsByClassName("prodect")[12].style.backgroundImage = ` url(${sre12[3]})`;
-    document.getElementsByClassName("prodect")[13].style.backgroundImage = ` url(${sre13[3]})`;
-    document.getElementsByClassName("prodect")[14].style.backgroundImage = ` url(${sre14[3]})`;
-    document.getElementsByClassName("prodect")[15].style.backgroundImage = ` url(${sre15[3]})`;
-    document.getElementsByClassName("prodect")[16].style.backgroundImage = ` url(${sre16[3]})`;
-    document.getElementsByClassName("prodect")[17].style.backgroundImage = ` url(${sre17[3]})`;
-    document.getElementsByClassName("prodect")[0].style.backgroundImage = ` url(${sre[3]})`;
-    
-}
-on.onclick= function () {
-    document.getElementsByClassName("prodect")[1].style.backgroundImage = ` url(${sre1[4]})`;
-    document.getElementsByClassName("prodect")[2].style.backgroundImage = ` url(${sre2[4]})`;
-    document.getElementsByClassName("prodect")[3].style.backgroundImage = ` url(${sre3[4]})`;
-    document.getElementsByClassName("prodect")[4].style.backgroundImage = ` url(${sre4[4]})`;
-    document.getElementsByClassName("prodect")[5].style.backgroundImage = ` url(${sre5[4]})`;
-    document.getElementsByClassName("prodect")[6].style.backgroundImage = ` url(${sre6[4]})`;
-    document.getElementsByClassName("prodect")[7].style.backgroundImage = ` url(${sre7[4]})`;
-    document.getElementsByClassName("prodect")[8].style.backgroundImage = ` url(${sre8[4]})`;
-    document.getElementsByClassName("prodect")[9].style.backgroundImage = ` url(${sre9[4]})`;
-    document.getElementsByClassName("prodect")[10].style.backgroundImage = ` url(${sre10[4]})`;
-    document.getElementsByClassName("prodect")[11].style.backgroundImage = ` url(${sre11[4]})`;
-    document.getElementsByClassName("prodect")[12].style.backgroundImage = ` url(${sre12[4]})`;
-    document.getElementsByClassName("prodect")[13].style.backgroundImage = ` url(${sre13[4]})`;
-    document.getElementsByClassName("prodect")[14].style.backgroundImage = ` url(${sre14[4]})`;
-    document.getElementsByClassName("prodect")[15].style.backgroundImage = ` url(${sre15[4]})`;
-    document.getElementsByClassName("prodect")[16].style.backgroundImage = ` url(${sre16[4]})`;
-    document.getElementsByClassName("prodect")[17].style.backgroundImage = ` url(${sre17[4]})`;
-    document.getElementsByClassName("prodect")[0].style.backgroundImage = ` url(${sre[4]})`;
-    
-}
 
 
 
-console.log(on)
+
--- js/m.js
+++ js/m.js
@@ -1,15 +1,40 @@
-let pro = document.querySelectorAll(".prodect");
 
 
-
-pro.forEach( ( div ) => {
+let pro2 = document.querySelectorAll(".pro1");
+pro2.forEach( ( div ) => {
 
     div.addEventListener( "click", ( e ) => {
 
-  window.open("../html/info prodect.html")
+  window.open("html/info prodect.html","_self")
 
     } )
 } )
+
+
+
+
+
+
+let pro3 = document.querySelectorAll(".carousel-inner");
+pro3.forEach( ( div ) => {
+
+    div.addEventListener( "click", ( e ) => {
+
+  window.open("html/info prodect.html","_self")
+
+    } )
+} )
+
+
+
+
+
+
+
+
+
+
+
 
 
 let next = document.querySelectorAll(".show a");
